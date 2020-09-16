dcc88f551f6d73a1ce2974aba4ae77a8f4ab734d
Stan Nieuwmans <snieuwmans@gmail.com>
2020-09-17 00:06:25 +0200

    added 'textfield' for search bar to

--- planapp/lib/main.dart
+++ planapp/lib/main.dart
@@ -36,14 +36,32 @@ class Home extends StatelessWidget {
           children: [
             Container(
                 child: Text(
-              "Pack your bag and create unbelievable adventures and explore the world",
-              style: TextStyle(fontSize: 40, fontFamily: 'RoboSlab'),
-              textAlign: TextAlign.left,
-            ),
-            padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0))
+                  "Pack your bag and create unbelievable adventures and explore the world",
+                  style: TextStyle(fontSize: 40, fontFamily: 'RoboSlab'),
+                  textAlign: TextAlign.left,
+                ),
+                padding:
+                    EdgeInsets.symmetric(horizontal: 35.0, vertical: 15.0)),
             Container(
-              child
-            )
+              child: TextFormField(
+                textAlign: TextAlign.left,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  hintText: 'What would you like to explore?',
+                  hintStyle: TextStyle(
+                    fontSize: 20.0,
+                    height: 2.5,
+                  ),
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.all(0.0),
+                    child: Icon(Icons.search, color: Colors.grey),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 0.0),
+                ),
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 35.0, vertical: 25.0),
+            ),
           ],
         ),
       ),
